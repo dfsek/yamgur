948ae30b5695f25c40463dd679afa8540e8bcdc8
dfsek <dfsek@protonmail.com>
2022-11-05 12:10:37 -0700

    clean up

--- src/Application.hs
+++ src/Application.hs
@@ -1,33 +1,33 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE DerivingStrategies #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE ViewPatterns #-}
 
 module Application where
 
-import ClassyPrelude.Yesod (newManager, Static, UTCTime, getCurrentTime, unpack, (</>), FileInfo(..), fromString, asText, pack)
+import ClassyPrelude.Yesod (FileInfo (..), Static, UTCTime, asText, fromString, getCurrentTime, newManager, pack, unpack, (</>))
+import Control.Monad.Logger (runStdoutLoggingT)
+import Data.Snowflake (Snowflake, SnowflakeGen, nextSnowflake, snowflakeToInteger)
+import Data.Yaml.Aeson (decodeFileEither)
+import Database.Persist.Sqlite (ConnectionPool, createSqlitePool)
 import GenericOIDC (oidcAuth')
+import System.Directory (createDirectoryIfMissing)
+import Text.Julius
+import URI.ByteString (Absolute, URIRef)
+import YamgurConfig
 import Yesod
 import Yesod.Auth
 import Yesod.Auth.OAuth2.Prelude
-import Prelude
-import YamgurConfig
-import Data.Yaml.Aeson (decodeFileEither)
-import System.Directory (createDirectoryIfMissing)
-import Yesod.Static
-import Control.Monad.Logger (runStdoutLoggingT)
-import Database.Persist.Sqlite (createSqlitePool, ConnectionPool)
-import Data.Snowflake (SnowflakeGen, nextSnowflake, Snowflake, snowflakeToInteger)
 import Yesod.Form.Bootstrap3
-import Text.Julius
-import URI.ByteString (URIRef, Absolute)
+import Yesod.Static
+import Prelude
 
 data Yamgur = Yamgur
   { httpManager :: Manager,
@@ -47,6 +47,19 @@ mkYesod
 /view/#Integer/#Text   ViewR GET
 |]
 
+css =
+  [cassius|
+body
+  margin: 40px auto
+  max-width: 650px
+  line-height: 1.6
+  font-size: 18px
+  color: #444
+  padding: 0 10px
+h1, h2, h3
+  line-height: 1.2
+|]
+
 instance Yesod Yamgur where
   approot = ApprootMaster $ host . config
   authRoute _ = Just $ AuthR LoginR
@@ -56,10 +69,10 @@ instance Yesod Yamgur where
 
 isSignedIn :: HandlerFor Yamgur AuthResult
 isSignedIn = do
-    user <- maybeAuthId
-    return $ case user of
-        Nothing -> AuthenticationRequired
-        Just _ -> Authorized
+  user <- maybeAuthId
+  return $ case user of
+    Nothing -> AuthenticationRequired
+    Just _ -> Authorized
 
 instance YesodAuth Yamgur where
   type AuthId Yamgur = Text
@@ -71,7 +84,6 @@ instance YesodAuth Yamgur where
 
 instance RenderMessage Yamgur FormMessage where
   renderMessage _ _ = defaultFormMessage
-
 
 fileInputId :: Text
 fileInputId = "file_input"
@@ -85,27 +97,41 @@ uploadMForm = renderBootstrap3 BootstrapBasicForm uploadAForm
 getHomeR :: Handler Html
 getHomeR = do
   user <- maybeAuthId
+  mmsg <- getMessage
   defaultLayout
-        [whamlet|
+    [whamlet|
+          ^{css}
+          <h1>Welcome to Yamgur!
+          <p>Yamgur is a free, minimal image upload/sharing server that you can host yourself.
+          <p>It is written in 
+            <a href="https://www.haskell.org/">Haskell
+            and uses the 
+            <a href="https://www.yesodweb.com/">Yesod
+            web framework.
           $maybe un <- user
             <p>Logged in as #{un}
             <p>
-              <a href=@{UploadR}>Upload Image
+              <a href=@{UploadR}>Upload
             <p>
-              <a href=@{AuthR LogoutR}>Logout
+              <a href=@{AuthR LogoutR}>Log out
           $nothing
             <p>
               <a href=@{AuthR LoginR}>Log in
+          $maybe msg <- mmsg
+            <p>#{msg}
         |]
-
 
 getViewR :: Integer -> Text -> Handler Html
 getViewR flake img = do
   yamgur <- getYesod
+  mmsg <- getMessage
   let baseURL = pack (unpack (host (config yamgur))) <> "/img/" <> show flake <> "/" <> unpack img
-  defaultLayout [whamlet|
-      <h1>
-        Image #{img}
+  defaultLayout
+    [whamlet|
+      ^{css}
+      <h1>Image #{img}
+      $maybe msg <- mmsg
+        <p>#{msg}
       <img src=#{baseURL}>
       <p>
         Permalink: <a href=#{baseURL}>#{baseURL}
@@ -113,14 +139,14 @@ getViewR flake img = do
         <a href=@{HomeR}>Home</a> <a href=@{UploadR}>Upload</a>
     |]
 
-
 getUploadR :: Handler Html
 getUploadR = do
   ((_, widget), enctype) <- runFormPost uploadMForm
   mmsg <- getMessage
 
   let form_id = "image_form" :: String
-      pasteScript = [julius|
+      pasteScript =
+        [julius|
   const form = document.getElementById(#{form_id});
   const fileInput = document.getElementById(#{fileInputId});
 
@@ -129,45 +155,39 @@ getUploadR = do
   |]
 
   defaultLayout $ do
-          [whamlet|$newline never
+    [whamlet|$newline never
+  ^{css}
+  <h1>Upload Image
   $maybe msg <- mmsg
-      <div .message>
-          <div .container>
-              #{msg}
-  <div .container>
-      <div .row>
-          <h2>
-              Upload new image
-          <div .form-actions>
-              <form method=post enctype=#{enctype} id=#{form_id}>
-                  ^{widget}
-                  <input .btn type=submit value="Upload">
-              ^{pasteScript}
+    <p>#{msg}
+  <form method=post enctype=#{enctype} id=#{form_id}>
+    ^{widget}
+    <input .btn type=submit value="Upload">
+  ^{pasteScript}
 
   |]
 
 postUploadR :: Handler Html
 postUploadR = do
-    ((result, _), _) <- runFormPost uploadMForm
-    case result of
-        FormSuccess file -> do
-            -- save to image directory
-            yamgur <- getYesod
-            flake <- liftIO $ nextSnowflake (snowflakeGen yamgur)
-            let filename = unpack $ fileName file
-                path = content_directory (config yamgur) </> show flake </> filename
+  ((result, _), _) <- runFormPost uploadMForm
+  case result of
+    FormSuccess file -> do
+      -- save to image directory
+      yamgur <- getYesod
+      flake <- liftIO $ nextSnowflake (snowflakeGen yamgur)
+      let filename = unpack $ fileName file
+          path = content_directory (config yamgur) </> show flake </> filename
 
-            liftIO $ do
-              createDirectoryIfMissing True $ content_directory (config yamgur) </> show flake
-              fileMove file path
-            let uploadPath = unpack (host (config yamgur)) <> "/" <> path
-            liftIO $ putStrLn $ "Saved image as " <> uploadPath
-            setMessage "Image saved"
-            redirect $ ViewR (snowflakeToInteger flake) (fileName file)
-        _ -> do
-            setMessage "Something went wrong"
-            redirect UploadR
-
+      liftIO $ do
+        createDirectoryIfMissing True $ content_directory (config yamgur) </> show flake
+        fileMove file path
+      let uploadPath = unpack (host (config yamgur)) <> "/" <> path
+      liftIO $ putStrLn $ "Saved image as " <> uploadPath
+      setMessage "Image saved"
+      redirect $ ViewR (snowflakeToInteger flake) (fileName file)
+    _ -> do
+      setMessage "Something went wrong"
+      redirect UploadR
 
 appMain :: IO ()
 appMain = do
